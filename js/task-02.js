ca17ad09c2e505f17dcd1e5739367ea71c964249
TryHungury <mukolanadebernuy@gmail.com>
2022-10-08 13:02:15 +0300

    Виконав другу тасочку

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulRef = document.querySelector('#ingredients');
+console.log(ulRef);
+
+function makeListName(ingredientsName) {
+  // let nodeRef = ulRef;
+
+  ingredientsName.forEach(name =>  {
+    // console.log(name);
+    
+    const liItem = document.createElement('li');
+    liItem.textContent = name;
+    liItem.classList.add('item')
+    
+    ulRef.append(liItem);
+    // console.log(ulRef);
+  })
+
+}
+
+makeListName(ingredients);
