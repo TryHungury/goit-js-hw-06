fce132b5903018df643db7f3ea374a42c385c011
TryHungury <mukolanadebernuy@gmail.com>
2022-10-08 19:42:07 +0300

    Виконав дев'яту тасочку

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+// console.log("btnRef", btnRef)
+
+const spanRef = document.querySelector('.color');
+// console.log("spanRef", spanRef)
+
+const changeColor = () => {
+  let randomColor = getRandomHexColor()
+  document.body.style.backgroundColor = randomColor;
+
+  spanRef.textContent = randomColor;
+  // console.log(randomColor);
+}
+
+btnRef.addEventListener('click', changeColor);
